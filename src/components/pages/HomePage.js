1a6bad91d1a1a2dd270bc6e8e978768d4cb9006b
LeonMesquita <leonmesquita16@gmail.com>
2022-07-06 16:16:45 -0300

    suggestions bar

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,10 +5,44 @@ export default function HomePage(){
     const [searchedProduct, setSearchedProduct] = useState('');
     //#D49943
     //#F5C974
+    const productTypeList = ['Tudo', 'Camisetas', 'Canecas', 'Funkos', 'Actions'];
     return(
         <>
             <SearchBar value={searchedProduct} setValue={setSearchedProduct}/>
+            <SuggestionsArea>
+                {productTypeList.map((type) => <button>{type}</button>)}
+            </SuggestionsArea>
         </>
     );
 }
 
+const SuggestionsArea = styled.div`
+    height: 35px;
+    width: 100%;
+    background: #D69A44;
+    display: flex;
+    margin-top: 50px;
+    align-items: center;
+    justify-content: space-evenly;
+    overflow: scroll;
+    
+    button{
+        border: none;
+        background: transparent;
+        color: white;
+        font-weight: 700;
+        font-size: 17px;
+        cursor: pointer;
+        transition: font-size 0.2s;
+
+        &:hover{
+       
+            font-size: 20px;
+
+    }
+
+    }
+
+
+
+`
