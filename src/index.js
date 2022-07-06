e5e334ec4afc1348bb5cab86aab64ef4ab11ce83
LeonMesquita <leonmesquita16@gmail.com>
2022-07-06 13:17:38 -0300

    configurações iniciais

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import  ReactDOM from "react-dom";
 import App from "./components/App";
-//import './reset.css';
-//import './style.css';
+import './reset.css';
+import './styles.css';
 ReactDOM.render(<App />, document.querySelector(".root"));
