02cb715c515dfe3ca3d2a856065e0dc78afd8ebc
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 18:57:20 -0300

    refat

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 import HomePage from "./pages/HomePage";
-
+import { useState } from "react";
+import Context from "../Context";
 export default function App(){
+    //const apiUrl = 'http://localhost:5000/'
+    const apiUrl = "https://darkside-store-api.herokuapp.com/";
+    const [token, setToken] = useState('');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
     return(
         <div className="main-container">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HomePage />}/>
-                </Routes>
-            </BrowserRouter>
+            <Context.Provider value={{token, setToken, apiUrl, authorization}}>
+                <BrowserRouter>
+                <NavBar />
+                    <Routes>
+                        <>
+                    
+                        <Route path="/" element={<HomePage />}/>
+                        </>
+                        
+                    </Routes>
+                </BrowserRouter>
+            </Context.Provider>
         </div>
     );
 };
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,74 @@
+import { useState, useContext, useEffect } from "react";
+import Context from '../../Context';
+import styled from 'styled-components';
+import ConfirmationButton from "../ConfirmationButton";
+import ProductCard from "../ProductCard";
+import SearchBar from "../SearchBar";
+import SuggestionsArea from "../styled/SuggestionsArea";
+import axios from 'axios';
 export default function HomePage(){
+    const [searchedProduct, setSearchedProduct] = useState('');
+    const [itemsQuantity, setItemsQuantity] = useState(0);
+    const {token, setToken, apiUrl, authorization} = useContext(Context);
+    const [productsList, setProductsList] = useState([]);
+    
+    //#D49943
+    //#F5C974
+    const productTypeList = ['Tudo', 'Camisetas', 'Canecas', 'Funkos', 'Actions'];
+
+
+
+    
+
+    async function getProducts(){
+        try{
+            const promise = await axios.get(`${apiUrl}products`);
+            console.log(promise.data);
+            setProductsList(promise.data);
+
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+
+    }, []);
+
+
+
+
+    function addItem(){
+        setItemsQuantity(itemsQuantity+1);
+    }
+    function removeItem(){
+        if (itemsQuantity > 0)
+        setItemsQuantity(itemsQuantity-1);
+    }
     return(
-        <></>
+        <>
+            <SearchBar value={searchedProduct} setValue={setSearchedProduct}/>
+            <SuggestionsArea>
+                {productTypeList.map((type, index) => <button key={index}>{type}</button>)}
+            </SuggestionsArea>
+            <ProductsArea>
+                {productsList.map((product, index) => 
+                <ProductCard key={index} src={product.image} title={product.title}
+                price={product.price} quantity={itemsQuantity} add={addItem} remove={removeItem}/>)}
+            </ProductsArea>
+            <div className="sized-box"></div>
+            <ConfirmationButton />
+        </>
     );
 }
+
+
+const ProductsArea = styled.div`
+    margin-top: 40px;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+`
