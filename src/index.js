6e780edaa12855fcc24ae70f8d3a9b365c750b91
luigi tanaka <luigi.tanaka12@gmail.com>
2022-07-09 11:37:56 -0300

    arquivos .css em pasta separada

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import  ReactDOM from "react-dom";
-import './reset.css';
-import './styles.css';
+import './css/reset.css';
+import './css/styles.css';
 import App from "./components/App";
 ReactDOM.render(<App />, document.querySelector(".root"));
