0ec0a2a2a8464821979332a4b509bb4710d58c11
luigi tanaka <luigi.tanaka12@gmail.com>
2022-07-10 15:00:25 -0300

    resumo dos produtos selecionados

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -6,8 +6,6 @@ import ProductCheckout from "../ProductCheckout";
 
 export default function Checkout() {
 
-    const [carregando, setCarregando] = useState(false);
-
     const [street, setStreet] = useState("");
     const [city, setCity] = useState("");
     const [state, setState] = useState("");
@@ -32,7 +30,7 @@ export default function Checkout() {
         },
         {
             _id: 2,
-            title: "Caneca Star Wars",
+            title: "Caneca Star Warsssssssssssssssssssssssssss",
             price: "29,90",
             image: "https://static3.tcdn.com.br/img/img_prod/460977/caneca_star_wars_logo_preto_e_amarelo_64693_1_20201211171758.jpeg",
             quantity: 2
@@ -45,74 +43,83 @@ export default function Checkout() {
         );
     }
 
-    //teste
-    const total = 250;
+    function calculateTotal() {
+        let total = 0;
+        cartList.forEach((product) => {
+            const priceInNumber = parseFloat(product.price.replace(',', '.'));
+            total += priceInNumber * product.quantity;
+        })
+
+        return total.toFixed(2);
+    }
+
+    const total = calculateTotal();
 
     function createFormCheckout() {
-        if (!carregando) {
-            return (
-                <>
-                    <h1>Endereço da entrega</h1>
+        return (
+            <>
+                <h1>Endereço da entrega</h1>
 
-                    <Inputs>
-                        <label for="street">Endereço: </label>
-                        <input id="street" type="text" placeholder="Seu endereço..." value={street} onChange={e => setStreet(e.target.value)} required />
-                    </Inputs>
-                    <Inputs>
-                        <label for="city">Cidade: </label>
-                        <input id="city" type="text" placeholder="Sua cidade..." value={city} onChange={e => setCity(e.target.value)} required />
-                    </Inputs>
-                    <Inputs>
-                        <label for="state">Estado: </label>
-                        <select id="state" name="state" value={state} onChange={e => setState(e.target.value)}>
-                            <option value="AC">Acre</option>
-                            <option value="AL">Alagoas</option>
-                            <option value="AP">Amapá</option>
-                            <option value="AM">Amazonas</option>
-                            <option value="BA">Bahia</option>
-                            <option value="CE">Ceará</option>
-                            <option value="DF">Distrito Federal</option>
-                            <option value="ES">Espírito Santo</option>
-                            <option value="GO">Goiás</option>
-                            <option value="MA">Maranhão</option>
-                            <option value="MT">Mato Grosso</option>
-                            <option value="MS">Mato Grosso do Sul</option>
-                            <option value="MG">Minas Gerais</option>
-                            <option value="PA">Pará</option>
-                            <option value="PB">Paraíba</option>
-                            <option value="PR">Paraná</option>
-                            <option value="PE">Pernambuco</option>
-                            <option value="PI">Piauí</option>
-                            <option value="RJ">Rio de Janeiro</option>
-                            <option value="RN">Rio Grande do Norte</option>
-                            <option value="RS">Rio Grande do Sul</option>
-                            <option value="RO">Rondônia</option>
-                            <option value="RR">Roraima</option>
-                            <option value="SC">Santa Catarina</option>
-                            <option value="SP">São Paulo</option>
-                            <option value="SE">Sergipe</option>
-                            <option value="TO">Tocantins</option>
-                            <option value="EX">Estrangeiro</option>
-                        </select>
-                    </Inputs>
-                    <Inputs>
-                        <label for="CEP">CEP: </label>
-                        <input id="CEP" type="text" placeholder="Seu CEP..." value={CEP} onChange={e => setCEP(e.target.value)} required />
-                    </Inputs>
+                <Inputs>
+                    <label for="street">Endereço: </label>
+                    <input id="street" type="text" placeholder="Seu endereço..." value={street} onChange={e => setStreet(e.target.value)} required />
+                </Inputs>
+                <Inputs>
+                    <label for="city">Cidade: </label>
+                    <input id="city" type="text" placeholder="Sua cidade..." value={city} onChange={e => setCity(e.target.value)} required />
+                </Inputs>
+                <Inputs>
+                    <label for="state">Estado: </label>
+                    <select id="state" name="state" value={state} onChange={e => setState(e.target.value)}>
+                        <option value="AC">Acre</option>
+                        <option value="AL">Alagoas</option>
+                        <option value="AP">Amapá</option>
+                        <option value="AM">Amazonas</option>
+                        <option value="BA">Bahia</option>
+                        <option value="CE">Ceará</option>
+                        <option value="DF">Distrito Federal</option>
+                        <option value="ES">Espírito Santo</option>
+                        <option value="GO">Goiás</option>
+                        <option value="MA">Maranhão</option>
+                        <option value="MT">Mato Grosso</option>
+                        <option value="MS">Mato Grosso do Sul</option>
+                        <option value="MG">Minas Gerais</option>
+                        <option value="PA">Pará</option>
+                        <option value="PB">Paraíba</option>
+                        <option value="PR">Paraná</option>
+                        <option value="PE">Pernambuco</option>
+                        <option value="PI">Piauí</option>
+                        <option value="RJ">Rio de Janeiro</option>
+                        <option value="RN">Rio Grande do Norte</option>
+                        <option value="RS">Rio Grande do Sul</option>
+                        <option value="RO">Rondônia</option>
+                        <option value="RR">Roraima</option>
+                        <option value="SC">Santa Catarina</option>
+                        <option value="SP">São Paulo</option>
+                        <option value="SE">Sergipe</option>
+                        <option value="TO">Tocantins</option>
+                        <option value="EX">Estrangeiro</option>
+                    </select>
+                </Inputs>
+                <Inputs>
+                    <label for="CEP">CEP: </label>
+                    <input id="CEP" type="text" placeholder="Seu CEP..." value={CEP} onChange={e => setCEP(e.target.value)} required />
+                </Inputs>
 
-                    <hr></hr>
+                <hr></hr>
 
-                    <h1>Pagamento</h1>
-                    <Inputs>
-                        <label for="cardNumber">N° do cartão: </label>
-                        <input id="cardNumber" type="text" placeholder="Número do cartão..." value={cardNumber} onChange={e => setCardNumber(e.target.value)} required />
-                    </Inputs>
-                    <Inputs>
-                        <label for="name">Nome: </label>
-                        <input id="name" type="text" placeholder="Nome do titular..." value={name} onChange={e => setName(e.target.value)} required />
-                    </Inputs>
-                    <Inputs className="validade">
-                        <label>Validade: </label>
+                <h1>Pagamento</h1>
+                <Inputs>
+                    <label for="cardNumber">N° do cartão: </label>
+                    <input id="cardNumber" type="text" placeholder="Número do cartão..." value={cardNumber} onChange={e => setCardNumber(e.target.value)} required />
+                </Inputs>
+                <Inputs>
+                    <label for="name">Nome: </label>
+                    <input id="name" type="text" placeholder="Nome do titular..." value={name} onChange={e => setName(e.target.value)} required />
+                </Inputs>
+                <Inputs>
+                    <label>Validade: </label>
+                    <Validate>
                         <select id="monthValidade" name="monthValidade" value={monthValidade} onChange={e => setMonthValidade(e.target.value)}>
                             <option value="01">Janeiro</option>
                             <option value="02">Fevereiro</option>
@@ -138,25 +145,31 @@ export default function Checkout() {
                             <option value="29">29</option>
                             <option value="30">30</option>
                         </select>
-                    </Inputs>
-                    <Inputs>
-                        <label for="CVV">CVV: </label>
-                        <input id="CVV" type="text" placeholder="Código de segurança..." value={CVV} onChange={e => setCVV(e.target.value)} required />
-                    </Inputs>
-                    <Inputs>
-                        <label for="installments">Parcelas: </label>
-                        <select id="installments" name="installments" value={installments} onChange={e => setInstallments(e.target.value)}>
-                            <option value="1x">1x de {total.toFixed(2)}</option>
-                            <option value="2x">2x de {(total/2).toFixed(2)}</option>
-                            <option value="3x">3x de {(total/3).toFixed(2)}</option>
-                            <option value="4x">4x de {(total/4).toFixed(2)}</option>
-                            <option value="5x">5x de {(total/5).toFixed(2)}</option>
-                            <option value="6x">6x de {(total/6).toFixed(2)}</option>
-                        </select>
-                    </Inputs>
-                </>
-            );
-        }
+                    </Validate>
+                </Inputs>
+                <Inputs>
+                    <label for="CVV">CVV: </label>
+                    <input id="CVV" type="text" placeholder="Código de segurança..." value={CVV} onChange={e => setCVV(e.target.value)} required />
+                </Inputs>
+                <Inputs>
+                    <label for="installments">Parcelas: </label>
+                    <select id="installments" name="installments" value={installments} onChange={e => setInstallments(e.target.value)}>
+                        <option value="1x">1x de {total.replace(".", ",")}</option>
+                        <option value="2x">2x de {(total / 2).toFixed(2).replace(".", ",")}</option>
+                        <option value="3x">3x de {(total / 3).toFixed(2).replace(".", ",")}</option>
+                        <option value="4x">4x de {(total / 4).toFixed(2).replace(".", ",")}</option>
+                        <option value="5x">5x de {(total / 5).toFixed(2).replace(".", ",")}</option>
+                        <option value="6x">6x de {(total / 6).toFixed(2).replace(".", ",")}</option>
+                    </select>
+                </Inputs>
+                <button type="submit">Finalizar compra</button>
+            </>
+        );
+    }
+
+    function finalizeOrder(event) {
+        event.preventDefault();
+        console.log("terminarr");
     }
 
     const products = showProducts();
@@ -167,9 +180,17 @@ export default function Checkout() {
             <Summary>
                 <h1>Produtos selecionados</h1>
                 {products}
+                <Shipping>
+                    <h5>Frete:</h5>
+                    <h5>Frete Grátis</h5>
+                </Shipping>
+                <Total>
+                    <h5>Total da compra:</h5>
+                    <h5>R${total.replace(".", ",")}</h5>
+                </Total>
             </Summary>
             <FormCheckout>
-                <form>
+                <form onSubmit={finalizeOrder}>
                     {formCheckout}
                 </form>
             </FormCheckout>
@@ -184,11 +205,7 @@ const Container = styled.div`
     padding: 20px 10px;
     width: 550px;
 
-    h1 {
-        font-size: 30px;
-        color: #E19F41;
-        margin-bottom: 20px;
-    }
+    
 
     @media(max-width: 550px) {
         width: 100%;
@@ -196,7 +213,38 @@ const Container = styled.div`
 `
 
 const Summary = styled.div`
+    margin-bottom: 40px;
 
+    >h1 {
+        font-size: 30px;
+        color: #E19F41;
+        margin-bottom: 20px;
+    }
+`
+
+const Shipping = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    background-color: #FCCB6F;
+    color: #06222d;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 6px; 
+    margin-bottom: 8px; 
+    box-shadow: 0px 10px 18px 0px rgba(0,0,0,0.3); 
+`
+
+const Total = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    background-color: #FCCB6F;
+    color: #06222d;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 6px;
+    box-shadow: 0px 10px 18px 0px rgba(0,0,0,0.3);
 `
 
 const Inputs = styled.div`
@@ -210,10 +258,17 @@ const Inputs = styled.div`
 `
 
 const FormCheckout = styled.div`
-    border: 1px solid blue;
     background-color: rgba(255, 255, 255, 0.9);
     padding: 10px 20px 10px 10px;
     border-radius: 3px;
+    box-shadow: 0px 10px 18px 0px rgba(0,0,0,0.3);
+
+    h1 {
+        font-size: 30px;
+        color: #051731;
+        margin-bottom: 28px;
+        font-weight: bold;
+    }
 
     label {
         font-weight: bold;
@@ -237,15 +292,42 @@ const FormCheckout = styled.div`
         }
     }
 
-    .validade {
-        select {
-            width: 180px;
-            margin-right: 10px;
+    form {
+        width: 100%;
+    }
 
-            &:last-child {
-                width: 120px;
-                margin-right: 0;
-            }
+    button {
+        border: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 26px;
+        width: 260px;
+        height: 46px;
+        margin: 30px auto 10px auto;
+        padding: 10px;
+        background-color: #FCCB6F;
+        border-radius: 5px;
+        color: #051731;
+        font-weight: bold;
+        box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.2);
+
+        &:hover {
+            color: #2a7b9e;
+        }
+    }
+`
+
+const Validate = styled.div`
+    display: flex;
+    width: 100%;
+    select {
+        width: 70%;
+        margin-right: 10px;
+
+        &:last-child {
+            width: 30%;
+            margin-right: 0;
         }
     }
 `
