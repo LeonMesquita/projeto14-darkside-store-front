c6af667bd4d33db826c3fd0423323a87c72097c9
LeonMesquita <leonmesquita16@gmail.com>
2022-07-10 17:05:09 -0300

    refat login

--- src/components/pages/SignUpPage.js
+++ src/components/pages/SignUpPage.js
@@ -5,6 +5,9 @@ import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../../images/Death Star.png";
 import { ThreeDots } from 'react-loader-spinner';
+import {Body, Container, Logomarca} from '../LoginStyled.js';
+
+
 
 export default function SignIn() {
 
@@ -62,6 +65,7 @@ export default function SignIn() {
     const formSignUp = makeFormSignUp();
 
     return (
+        <div className="available-area">
         <Body>
             <Logomarca>
                 <img src = {Logo} />
@@ -70,98 +74,13 @@ export default function SignIn() {
             </Logomarca>
             
             <Container>
+            <h3>Junte-se ao lado negro da força!</h3>
+
                 {formSignUp}
                 <Link to = "/"><h5>Já possui uma conta? <strong>Entre aqui</strong></h5> </Link>
             </Container>
         </Body>
+        </div>
     )
 }
 
-const Body = styled.div `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-`
-const Container = styled.div `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    background-color: rgba(255, 255, 255, 0.4);
-    width: 330px;
-    border-radius: 10px;
-    padding: 20px 0 30px 0;
-    margin-top: 40px;
-
-    input {
-        width: 90%;
-        height: 40px;
-        background-color: rgba(255, 255, 255, 0.8);
-        border-radius: 5px;
-        margin-bottom: 10px;
-        color: #051731;
-        font-size: 20px;
-
-        &::placeholder {
-            font-family: 'Lexend Mega';
-            font-weight: 400;
-            font-size: 20px;
-            color: #ADADAD;
-        }
-
-        &:disabled {
-            background-color: #F2F2F2;
-            color: #AFAFAF;
-        }
-    }
-
-    strong {
-        color: #FCCB6F;
-    }
-
-    button {
-        width: 90%;
-        height: 40px;
-        background-color: #FCCB6F;
-        color: #051731;
-        border-radius: 5px;
-        margin-top: 10px;
-        margin-bottom: 5px;
-        border: none;
-        font-size: 18px;
-        font-weight: bold;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        &:disabled {
-            opacity: 0.7;
-        }
-    }
-
-    h5 {
-        color: white;
-        font-size: 14px;
-    }
-`
-
-const Logomarca = styled.div `
-    display: flex;    
-    position: relative;
-    margin-top: 120px;
-
-    h1{
-        color: white;
-        font-size: 20px;
-        letter-spacing: 5px;
-    }
-    h2{
-        padding-top: 20px;
-        padding-left: 38px;
-        color: #E19F41;
-        letter-spacing: 21px;
-        position: absolute;
-    }
-
-`
